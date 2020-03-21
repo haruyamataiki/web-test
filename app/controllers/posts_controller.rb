15459fbf4768ae2cc79c65f366d303566a630916
haruyamataiki <haruyamataiki619@gmali.com>
2020-03-21 10:24:33 +0000

    Add:post_comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,13 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		@post_comments = @post.post_comments
+		@post_comment = PostComment.new
 	end
 
 	def create
 		post = Post.new(post_params)
+		post.user_id = current_user.id
 		post.save
 		redirect_to posts_path
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_060313) do
+ActiveRecord::Schema.define(version: 2020_03_21_081747) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
