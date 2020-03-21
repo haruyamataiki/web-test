78267b41b305f8e43f66bb28324c37b647671389
haruyamataiki <haruyamataiki619@gmali.com>
2020-03-21 07:14:03 +0000

    Add:post_show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-		@posts = Post.new
+		@post = Post.new
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def create
+		post = Post.new(post_params)
+		post.save
+		redirect_to posts_path
 	end
 
  private
